140d6aeb02f267e86c1e9357ddb2fa815becc143
Ваше Имя <you@example.com>
2020-04-27 17:13:44 +0300

    Prepare 3.0.0 release

--- lib/rpush/version.rb
+++ lib/rpush/version.rb
@@ -1,3 +1,14 @@
 module Rpush
-  VERSION = '3.0.0'
+  module VERSION
+    MAJOR = 3
+    MINOR = 0
+    TINY = 0
+    PRE = nil
+
+    STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".").freeze
+
+    def self.to_s
+      STRING
+    end
+  end
 end
